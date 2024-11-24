51ee5a253d15c7ec45a521da9fa90798fe4b7b6b
frankruszel <110735562+frankruszel@users.noreply.github.com>
2024-11-25 02:24:04 +0800

    update code to test functionality

--- lambdas/github-to-lambda-02/index.js
+++ lambdas/github-to-lambda-02/index.js
@@ -1,7 +1,7 @@
 import * as lodash from 'lodash';
 
 export const handler = async () => {
-  const max = 20;
+  const max = 10;
   const val = lodash.random(max);
   const response = {
     statusCode: 200,
