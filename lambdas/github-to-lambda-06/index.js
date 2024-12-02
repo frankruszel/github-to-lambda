3d6373863fcee539b0f9b3499e720a587b17ac02
frankruszel <110735562+frankruszel@users.noreply.github.com>
2024-12-02 10:51:00 +0800

    feat: deploy lambda ONLY WHEN code changed

--- lambdas/github-to-lambda-06/index.js
+++ lambdas/github-to-lambda-06/index.js
@@ -5,7 +5,7 @@ export const handler = async () => {
   const val = lodash.random(max);
   const response = {
     statusCode: 200,
-    body: `06 new2: my random value (max: ${max}): ${val}`,
+    body: `06 new42: my random value (max: ${max}): ${val}`,
   };
   return response;
 };
